244447047acd4df8810eb7872d898824cb7778b9
elautrup <Eddie Lautrup>
2025-08-30 07:16:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Edward Lautrup
+ *         elautrup2@husker.unl.edu
+ * Date: 08/25/2025
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello, I'm Eddie Lautrup\n");
+  printf("My major is eletrical engineering.\n");
+
+  return 0;
+}
