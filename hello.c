8ba4810b94a922d8ec36a2026d7a6bbb183818a0
JacobAshman <123654887+JacobAshman@users.noreply.github.com>
2025-09-02 12:47:45 -0500

    test

--- hello.c
+++ hello.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
   printf("Hello, I'm Eddie Lautrup!\n");
   printf("My major is eletrical engineering.\n");
   printf("My minor is mathematics.\n");
-
+      printf("Hello! My name is Jacob Ashman!!\n");
+    printf("My major is EE & CE!\n");
   return 0;
 }
